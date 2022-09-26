1038b8ed979cfec3de696531eb21be6d6a6372b4
Iqbal Alfikri <iqbalalfikri12@gmail.com>
2022-09-26 14:48:56 +0700

    add rest api

--- producer/producer.go
+++ producer/producer.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gin-gonic/gin"
 	"github.com/nsqio/go-nsq"
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -14,6 +16,43 @@ type Message struct {
 }
 
 func main() {
+
+	router := gin.Default()
+	router.GET("/", publishMessage)
+	router.GET("/defer", publishDeferMessage)
+
+	router.Run(":8080")
+}
+
+func publishDeferMessage(c *gin.Context) {
+	config := nsq.NewConfig()
+
+	producer, err := nsq.NewProducer("localhost:4150", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic := "topic_golang_nsq"
+	msg := Message{
+		Name:      "Message golang nsq",
+		Content:   "Content golang nsq deferred",
+		Timestamp: time.Now().String(),
+	}
+
+	payload, err := json.Marshal(msg)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = producer.DeferredPublish(topic, 10*time.Second, payload)
+	if err != nil {
+		log.Println(err)
+	}
+
+	c.JSON(http.StatusOK, "ok")
+}
+
+func publishMessage(c *gin.Context) {
 	config := nsq.NewConfig()
 
 	producer, err := nsq.NewProducer("localhost:4150", config)
@@ -37,4 +76,6 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
+	c.JSON(http.StatusOK, "ok")
 }
